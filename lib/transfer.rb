0d8c78ca1beb1d502237e1d3410c5e5ad0098189
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-04-04 03:02:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,4 +26,11 @@ class Transfer
       "Transaction rejected. Please check your account balance."
     end
   end
+
+  def reverse_transfer
+    if valid? && sender.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+
+  end
 end
